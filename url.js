c7cfbc57283b1b1f1354fb15496a5f8a90dbb928
lolikonn <103613832+lolikonn@users.noreply.github.com>
2022-04-18 14:42:58 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,12 +1,3 @@
-/*
- * 將要隨機派發的網址放入下方（不需排序）
- *
- * 並請注意：
- * 1. 除最後一項外，其餘項目用逗號分隔
- * 2. 網址請用引號包起來
- */
-
-
 const urls = [
 	'https://docs.google.com/forms/d/1s6IzHJgtZmPy2UrhKZPgRegzLGUAHtq99aLuKq6V46U/edit?fbclid=IwAR1ItaZ2zLnzVaeXmEKu-y_artO43RfjQUTjvsPSIPAFa0WWF6LJKVJswyM',
 	'https://docs.google.com/forms/d/1s4VN_zi4AbZ4eGohYQTg1jSvI8MS851ST5ZJaR7U9CM/edit?fbclid=IwAR16oyYWZ963qGu1JpUy_BZXqaQP4oyzJNOkN2ASPMiJ4vPowbc1IHdKf6Q',
