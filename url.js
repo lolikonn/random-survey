f22d85a9d04cb559abc2e8ef4ef1c0ccf719f735
lolikonn <103613832+lolikonn@users.noreply.github.com>
2022-04-18 14:31:41 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://docs.google.com/forms/d/1s6IzHJgtZmPy2UrhKZPgRegzLGUAHtq99aLuKq6V46U/edit?fbclid=IwAR1ItaZ2zLnzVaeXmEKu-y_artO43RfjQUTjvsPSIPAFa0WWF6LJKVJswyM',
+	'https://docs.google.com/forms/d/1s4VN_zi4AbZ4eGohYQTg1jSvI8MS851ST5ZJaR7U9CM/edit?fbclid=IwAR16oyYWZ963qGu1JpUy_BZXqaQP4oyzJNOkN2ASPMiJ4vPowbc1IHdKf6Q',
+	'https://docs.google.com/forms/d/1loYEt_86E3CQXgdmoCG-51t-NvKIyyAjwIHbiRARTYo/edit?fbclid=IwAR0WTelqZGAfexJdkOIThrHSGdeueqZ4it2gd66YdKFuuLx6sMFf-3NxAIQ'
 ];
