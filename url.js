ea276db0d97120ef0c3217c607862a7cf1bf2d89
lolikonn <103613832+lolikonn@users.noreply.github.com>
2022-05-04 16:56:25 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,5 +1,5 @@
 const urls = [
-	'https://docs.google.com/forms/d/1s6IzHJgtZmPy2UrhKZPgRegzLGUAHtq99aLuKq6V46U/edit?fbclid=IwAR1ItaZ2zLnzVaeXmEKu-y_artO43RfjQUTjvsPSIPAFa0WWF6LJKVJswyM',
-	'https://docs.google.com/forms/d/1s4VN_zi4AbZ4eGohYQTg1jSvI8MS851ST5ZJaR7U9CM/edit?fbclid=IwAR16oyYWZ963qGu1JpUy_BZXqaQP4oyzJNOkN2ASPMiJ4vPowbc1IHdKf6Q',
-	'https://docs.google.com/forms/d/1loYEt_86E3CQXgdmoCG-51t-NvKIyyAjwIHbiRARTYo/edit?fbclid=IwAR0WTelqZGAfexJdkOIThrHSGdeueqZ4it2gd66YdKFuuLx6sMFf-3NxAIQ'
+	'https://forms.gle/J684HGVMm9QABQ3XA',
+	'https://forms.gle/9ntNGymPhFCwKM8K8',
+	'https://forms.gle/9SNmt9aBf1k1qJjM6'
 ];
